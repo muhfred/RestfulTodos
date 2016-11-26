e0e993538bf0789a1c5945761a7934f6dd0123cb
muhfred <muhfred@outlook.com>
2016-11-26 19:08:02 +0200

    name space issues

--- RestfulTodos/Models/ITodoRepository.cs
+++ RestfulTodos/Models/ITodoRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TodoWebApi.Models;
+using RestfulTodos.Models;
 
 namespace RestfulTodos.Models
 {
